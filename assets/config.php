7f30f3a5e9db8dcf17dbbe919f691d25a347a15f
Roshan Kumar <roshanraj69084@gmail.com>
2025-02-02 06:41:34 +0530

    hii

--- assets/config.php
+++ assets/config.php
@@ -2,7 +2,7 @@
     $server = "localhost";
    
     $user = "root";
-    $password = "";
+    $password = "Roshan@92";
     $db = "school";
     
     $conn = mysqli_connect($server, $user, $password, $db);
