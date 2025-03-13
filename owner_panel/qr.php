d0ebab15becea59ba75f26e690d97fc51fe27eb4
Roshan Kumar <roshanraj69084@gmail.com>
2025-03-13 12:55:40 +0530

    hh

--- owner_panel/qr.php
+++ owner_panel/qr.php
@@ -1,12 +1,10 @@
-
 <?php
-include("../assets/noSessionRedirect.php"); 
+include("../assets/noSessionRedirect.php");
 include('./fetch-data/verfyRoleRedirect.php');
+include("../assets/config.php");
 
-error_reporting(0);
-?>
-<?php
-   include("../assets/config.php");
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 ?>
 <!DOCTYPE html>
 <html>
@@ -17,93 +15,105 @@ error_reporting(0);
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <link rel="stylesheet" type="text/css" href="style.css">
     <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
-    <!-- <link rel="stylesheet" type="text/css" href="css/style.css"> -->
     <title>ERP</title>
-    <style type="text/css">
-      .payment{
-        margin-bottom: 10%;
-      }
-      @media (min-width: 1025px) {
-.h-custom {
-height: 100vh !important;
-}
-}
+    <style>
+        .payment {
+            margin-bottom: 10%;
+        }
+        @media (min-width: 1025px) {
+            .h-custom {
+                height: 100vh !important;
+            }
+        }
     </style>
 </head>
 <body>
-    <div class="header">
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="index.php">SCHOOL MANAGEMENT</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="index.php">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="notices.php">Notice</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Fee Pay
-          </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="make-payment.php">Make Payment</a></li>
-            <li><a class="dropdown-item" href="see-payment.php">See Payment</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="change-password.php">Change-Password</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="logout.php">Logout</a>
-        </li>
-      </ul>
-      <form class="d-flex">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    </div>
-  </div>
-</nav>
-    </div>
-
-    <div class="payment">
-      <section class="h-100 h-custom">
-  <div class="container py-5 h-100">
-    <div class="row d-flex justify-content-center align-items-center h-100">
-      <div class="col-lg-8 col-xl-6">
-        <div class="card rounded-3">
-          <div class="card-body p-4 p-md-5">
-            <h3 class="mb-4 pb-2 pb-md-0 mb-md-5 px-md-2">Payment Info</h3>
-
-            <form class="px-md-2">
-                <?php
-                   if($_POST['teacher_id']=='T1703597586'){
-                    echo "<img src='img/arzoo.jpg' style='width: 80%; margin-left: 10%;'>";
-                   }
-                   else if($_POST['teacher_id']=='T1703574415'){
-                       echo "<img src='img/qr2.jpg' style='width: 80%; margin-left: 10%;'>";
-                   }
-                   else{
-                    echo " No Qr Found ";
-                   }
-                ?>
-
-            </form>
-
-          </div>
+<div class="header">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="index.php">SCHOOL MANAGEMENT</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item"><a class="nav-link active" href="index.php">Home</a></li>
+                    <li class="nav-item"><a class="nav-link" href="notices.php">Notice</a></li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Fee Pay</a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="make-payment.php">Make Payment</a></li>
+                            <li><a class="dropdown-item" href="see-payment.php">See Payment</a></li>
+                            <li><hr class="dropdown-divider"></li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item"><a class="nav-link" href="change-password.php">Change Password</a></li>
+                    <li class="nav-item"><a class="nav-link" href="logout.php">Logout</a></li>
+                </ul>
+                <form class="d-flex">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+            </div>
         </div>
-      </div>
-    </div>
-  </div>
-</section>
-    </div>
+    </nav>
+</div>
 
-  </body>
-  </html>
+<div class="payment">
+    <section class="h-100 h-custom">
+        <div class="container py-5 h-100">
+            <div class="row d-flex justify-content-center align-items-center h-100">
+                <div class="col-lg-8 col-xl-6">
+                    <div class="card rounded-3">
+                        <div class="card-body p-4 p-md-5">
+                            <h3 class="mb-4 pb-2 pb-md-0 mb-md-5 px-md-2">Payment Info</h3>
+
+                            <form class="px-md-2" method="POST" action="">
+                                <label for="teacher_id">Enter Teacher ID:</label>
+                                <input type="text" name="teacher_id" id="teacher_id" class="form-control mb-3">
+                                <button type="submit" class="btn btn-primary">Submit</button>
+                            </form>
+
+                            <?php
+                            // Check if form is submitted
+                            if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['teacher_id'])) {
+                                $teacher_id = trim($_POST['teacher_id']); // Trim whitespace
+
+                                // Debugging Output
+                                echo "<p>Received Teacher ID: <strong>$teacher_id</strong></p>";
+
+                                // QR Code Display Logic
+                                if ($teacher_id == 'T1740783949') {
+                                    echo "<img src='img/qr2.jpg' style='width: 80%; margin-left: 10%;'>";
+                                } elseif ($teacher_id == 'T1718791191') {
+                                    echo "<img src='img/qr2.jpg' style='width: 80%; margin-left: 10%;'>";
+                                }elseif ($teacher_id == 'T1738461273') {
+                                    echo "<img src='img/qr2.jpg' style='width: 80%; margin-left: 10%;'>";
+                                } else {
+                                    echo "<p style='color: red; font-weight: bold;'>No QR Found</p>";
+                                }
+                            } else {
+                                echo "<p style='color: blue;'>Please enter a Teacher ID to display the QR code.</p>";
+                            }
+                            ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
+
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
